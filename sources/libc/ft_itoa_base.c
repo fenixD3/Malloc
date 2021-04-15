cb1802c546a6b21a22ebe5a94847961268e31409
fenixD3 <filipcev2@gmail.com>
2021-04-15 09:22:22 +0300

    repair itoa_base

--- sources/libc/ft_itoa_base.c
+++ sources/libc/ft_itoa_base.c
@@ -3,17 +3,18 @@
 void	ft_itoa_base(size_t num, char base, t_bool hex_prefix)
 {
 	unsigned char to_out;
+	unsigned char mod;
+	unsigned char div;
 
-	if (hex_prefix)
-		ft_putstr("0x");
-	while (num)
-	{
-		unsigned char mod = num % base;
-		if (mod > 9)
-			to_out = mod + ('A' - 10);
-		else
-			to_out = mod + '0';
-		num /= base;
-		write(1, &to_out, 1);
-	}
+	if (num >= base)
+		ft_itoa_base(num / base, base, hex_prefix);
+	else
+		if (hex_prefix)
+			ft_putstr("0x");
+	mod = num % base;
+	if (mod > 9)
+		to_out = mod + ('A' - 10);
+	else
+		to_out = mod + '0';
+	write(1, &to_out, 1);
 }
