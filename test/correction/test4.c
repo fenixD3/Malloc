ef63aa9f19e641fe02a87565a5d8b5e5646bfeeb
Denis Filiptsev <denis.filiptsev@ntprog.com>
2021-04-23 19:16:01 +0300

    chmod run_linux.sh

--- test/correction/test4.c
+++ test/correction/test4.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../inc/malloc.h"
+#include "malloc.h"
 #include <string.h>
 
 void	print(char *s)
